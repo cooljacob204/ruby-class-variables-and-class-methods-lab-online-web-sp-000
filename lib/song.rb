dc4d410b5729a7c6b04c8dc24ac745cfaad6420f
Jacob Feldman <jacobgfeld@gmail.com>
2018-11-07 19:02:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,16 +3,22 @@ require 'pry'
 class Song
   attr_reader :name, :artist, :genre
   @@count = 0
+<<<<<<< HEAD
   @@artists = []
   @@genres = []
   @@artist_count = {}
   @@genre_count = {}
+=======
+  @@artists = {}
+  @@genres = {}
+>>>>>>> 857543e73bb0d861397736f672484fd32f1603ab
   
   def initialize(name, artist, genre)
     @@count += 1
     @name = name
     @artist = artist
     @genre = genre
+<<<<<<< HEAD
     @@genres << genre #if !@@genres.include?(genre)
     @@artists << artist #if !@@artists.include?(artist)
     !!@@artist_count[artist] ? @@artist_count[artist] += 1 : @@artist_count[artist] = 1
@@ -37,6 +43,22 @@ class Song
 
   def self.genres
     @@genre_count.keys
+=======
+    binding.pry
+    @@artists[artist] ? @@artists[artist] += 1 : @@artists[artist] = 1
+  end
+  
+  def count
+    @@count
+  end
+  
+  def genre_count
+    GENRES.length
+  end
+  
+  def artists_count
+    GENRES.length
+>>>>>>> 857543e73bb0d861397736f672484fd32f1603ab
   end
   
 end
